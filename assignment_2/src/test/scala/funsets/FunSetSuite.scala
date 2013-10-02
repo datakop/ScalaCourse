49a2cc4b4345c105d6db58061486dfae29ee17f3
Boris Kopin <kopbob@gmail.com>
2013-10-02 19:17:23 +0400

    Implemented Basic Functions on Sets exercises| Added tests

--- assignment_2/src/test/scala/funsets/FunSetSuite.scala
+++ assignment_2/src/test/scala/funsets/FunSetSuite.scala
@@ -86,7 +86,7 @@ class FunSetSuite extends FunSuite {
    * Once you finish your implementation of "singletonSet", exchange the
    * function "ignore" by "test".
    */
-  ignore("singletonSet(1) contains 1") {
+  test("singletonSet(1) contains 1") {
     
     /**
      * We create a new instance of the "TestSets" trait, this gives us access
@@ -101,12 +101,43 @@ class FunSetSuite extends FunSuite {
     }
   }
 
-  ignore("union contains all elements") {
+  test("union contains all elements") {
     new TestSets {
       val s = union(s1, s2)
       assert(contains(s, 1), "Union 1")
       assert(contains(s, 2), "Union 2")
       assert(!contains(s, 3), "Union 3")
+    }
+  }
+  
+  
+  test("intersect contains only same elements") {
+    new TestSets {
+      val s = intersect(union(s1, s2), s1)
+      
+      assert(contains(s, 1), "intersect 1")
+      assert(!contains(s, 2), "intersect 2")
+      assert(!contains(s, 3), "intersect 3")
+    }
+  }
+  
+  test("diff test") {
+    new TestSets {
+      val s = diff(union(s1, s2), s1)
+      
+      assert(!contains(s, 1), "diff 1")
+      assert(contains(s, 2), "diff 2")
+      assert(!contains(s, 3), "diff 3")
+    }
+  }
+  
+  test("filter {1, 2, 3} by x>1") {
+    new TestSets {
+      val s = filter(union(union(s1, s2), s3), (x: Int) => x > 1)
+      
+      assert(!contains(s, 1), "filter 1")
+      assert(contains(s, 2), "filter 2")
+      assert(contains(s, 3), "filter 3")
     }
   }
 }
